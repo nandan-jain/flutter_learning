fb1cc959e30ad20fe32cdd435f28bb3b15e4d8f5
nandan-jain <annejain2@gmail.com>
2021-12-29 07:55:50 -0800

    Added images and text to categories

--- lib/home_page_comp/posts.dart
+++ lib/home_page_comp/posts.dart
@@ -1,17 +1,32 @@
 import 'package:flutter/material.dart';
 
 class CatgoryCircle extends StatelessWidget {
-  const CatgoryCircle({Key? key}) : super(key: key);
+  final String categoryName;
+  final String imageUrl;
+
+  CatgoryCircle({required this.categoryName, required this.imageUrl});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: EdgeInsets.all(4),
-        child: Container(
-          height: 100,
-          width: 100,
-          decoration: BoxDecoration(
-              shape: BoxShape.circle, color: Colors.white70),
+        padding: EdgeInsets.all(5),
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(4),
+              child: Container(
+                height: 90,
+                width: 90,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(image: AssetImage(imageUrl))),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 8),
+              child: Text(categoryName),
+            )
+          ],
         ));
   }
 }
@@ -19,11 +34,12 @@ class CatgoryCircle extends StatelessWidget {
 class Categories extends StatelessWidget {
   final String sectionLabel;
   final List categories = [
-    CatgoryCircle(),
-    CatgoryCircle(),
-    CatgoryCircle(),
-    CatgoryCircle(),
-    CatgoryCircle()
+    CatgoryCircle(categoryName: 'Men', imageUrl: 'images/men.png'),
+    CatgoryCircle(categoryName: 'Women', imageUrl: 'images/women.png'),
+    CatgoryCircle(categoryName: 'Men', imageUrl: 'images/men.png'),
+    CatgoryCircle(categoryName: 'Women', imageUrl: 'images/women.png'),
+    CatgoryCircle(categoryName: 'Men', imageUrl: 'images/men.png'),
+    CatgoryCircle(categoryName: 'Women', imageUrl: 'images/women.png'),
   ];
   Categories({required this.sectionLabel});
 
@@ -32,8 +48,7 @@ class Categories extends StatelessWidget {
     return Padding(
         padding: EdgeInsets.symmetric(vertical: 10),
         child: Container(
-            height: 125,
-            color: Colors.black12,
+            height: 150,
             child: ListView.builder(
                 itemCount: categories.length,
                 scrollDirection: Axis.horizontal,
